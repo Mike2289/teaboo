3a68b0b919fcfc860d8d9a1894a3fa3f738fa3ad
TeaBoo123 <116780909+TeaBoo123@users.noreply.github.com>
2022-10-29 14:58:14 +0700

    yes

---
+++ inde_orix.php
@@ -0,0 +1,393 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta charset="utf-8">
+<meta name="description" content="Mengelola Arsip Perusahaan" />
+<meta name="keywords" content="java archiving solusindo,java arsip,penyimpanan arsip,pemusnahan arsip"/>
+<meta name="author" content="BeeAnt" />
+
+<meta name="viewport" content="width=device-width, maximum-scale=1">
+<title>Tea Ipeka BSD</title>
+
+
+<link rel="icon" href="favicon.png" type="image/png">
+
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+<link href="css/style.css" rel="stylesheet" type="text/css"> 
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css"> 
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+ 
+<!--[if lt IE 9]>
+    <script src="js/respond-1.1.0.min.js"></script>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/html5element.js"></script>
+<![endif]-->
+ 
+</head>
+
+
+
+<body>
+<header id="header">
+	<div id="head" class="parallax" parallax-speed="2" style="background-position: 50% 0px;">
+	
+		<div class="top_left_cont zoomIn wow" id="logo"> 
+		<img class="img-circle" src="img/guy.jpg" alt="">
+			 
+              <h1><br/><br/><br/><br/><br/><br/></h1>
+
+
+		</div>
+	</div>
+
+</header>
+ 
+ 
+ 
+<!--Header_section-->
+<header id="header_wrapper">
+  <div class="container">
+    <div class="header_box">
+
+	  <nav class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+	    <div id="main-nav" class="collapse navbar-collapse navStyle">
+			<ul class="nav navbar-nav" id="mainNav">
+			  <li class="active"><a href="#header" class="scroll-link">Home</a></li>
+			  <li><a href="#aboutUs" class="scroll-link">About Us</a></li>
+			  <li><a href="#products" class="scroll-link">Products</a></li>
+			  <li><a href="#contact" class="scroll-link">Contact Us</a></li>
+			</ul>
+      </div>
+	 </nav>
+    </div>
+  </div>
+</header>
+<!--Header_section--> 
+
+
+
+
+<section id="aboutUs"><!--Aboutus-->
+<div class="inner_wrapper about-us aboutUs-container fadeInLeft animated wow">
+  <div class="container">
+    <h2>About Us</h2>
+	<h6>
+	<b><i>
+	CV Archiving Solusindo didirikan pada awal tahun 2015 oleh para profesional yang memiliki pengetahuan dan pengalaman dalam dunia pengelolaan dokumen atau arsip.
+	</b></i>
+	</h6>
+
+	<div class="row"> 
+	<div class="col-sm-6 col-md-6">
+	<h2>Visi</h2>
+	<p>
+	<li>
+	Untuk menjadi perusahaan yang unggul dalam menyediakan layanan pengelolaan, penyimpanan dan pemusnahan dokumen, serta memberikan nilai tambah kepada pelanggan kami.
+	</li>
+	</p>
+	</div>	
+	
+	<div class="col-sm-6 col-md-6">
+	<h2>Misi</h2>
+	<p>
+	<li>
+	Tetap menjadi perusahaan dengan pertumbuhan yang inovatif, kewirausahaan dan tanggung jawab sosial nasional yang memberikan nilai tambah produk dan layanan untuk Klien kami.
+	</li>
+	</p>
+	<p>
+	<li>
+	Membantu Klien untuk mencapai dan tahan keunggulan kompetitif di pasar melalui layanan yang sangat baik dan memenuhi standar tata kelola perusahaan yang baik.
+	</li>
+	</p>
+	</div>	
+
+	</div>
+	
+
+  </div> 
+</div>
+</section>
+<!--Aboutus--> 
+
+
+<!--Service-->
+<section  id="products">
+  <div class="container">
+    <h2>Our Products</h2>
+	<h6>
+	Arsip mempunyai peran penting dalam kelangsungan hidup organisasi baik organisasi pemerintah maupun swasta. Manfaat arsip bagi suatu organisasi antara lain berisi informasi yang berguna sebagai alat bukti atas terjadinya transaksi keuangan dan aktivitas-aktivitas yang terkait segi hukum, dan juga sebagai alat pertanggung jawaban manajemen atas transparasi aktivitas atas operasi bisnis yang telah terjadi. 
+	</h6>
+
+    <div class="service_wrapper">
+      <div class="row">
+        <div class="col-md-3">
+		<div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-tree"></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">User Experience</h3>
+            <p class="animated fadeInDown wow">
+			Kami memiliki pengalaman dalam melakukan Penyimpanan dan Perawatan Arsip, Pengelolaan informasi dalam bentuk Arsip, Pemusnahan Dokumen dan Digitalisasi Dokumen atau jasa scanning dan indexing</li>
+			</p>
+          </div>
+        </div>
+		
+		
+        <div class="col-md-3"> 
+		<div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-crosshairs"></i></span> </div> 
+		<div class="service_block">
+            <h3 class="animated fadeInUp wow">Facilities</h3>
+            <p class="animated fadeInDown wow">
+			Lokasi bebas banjir & mudah dijangkau, Sistem Keamanan 24 Jam dilengkapi CCTV, Rak penyimpanan yang memadai dilengkapi dengan fasilitas Pest /Rodent Control
+			</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+		<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-building-o "></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">End Users</h3>
+            <p class="animated fadeInDown wow">
+			Kami menyediakan penjemputan dan pengambilan arsip dan barang-barang Alat Tulis Kantor lainnya serta program aplikasi yang membantu pelanggan dalam manajemen arsip
+			</p>
+          </div>
+        </div>
+		  <div class="col-md-3">
+		<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-twitter"></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">Consultation</h3>
+            <p class="animated fadeInDown wow">
+			Kami memberikan konsultasi gratis kepada perusahaan/organisasi dalam manajemen pengarsipan agar kelak mampu memperlihatkan bukti otentik secara cepat kepada pihak terkait (misalnya: pihak pajak, hukum atau badan pemeriksa lainnya yang membutuhkan bukti otentik)			
+			</p>
+          </div>
+        </div>
+      </div> 
+	 
+    </div>
+  </div>
+</section>
+
+
+
+
+<!--Service--> 
+<section id="techSkills">
+<div class="container">
+ <div class="row"> 
+<div class="col-sm-12 col-md-12">
+<h2>Pengertian Umum</h2>
+<p>
+Arsip ialah Penyimpanan dokumen dalam bentuk hardcopy atau digitial yang sudah tidak sering atau aktif di akses lagi dan disimpan dengan tujuan berjaga-jaga atau memenuhi peraturan perusahaan (misalnya audit) atau ketentuan hukum yang berlaku (misalnya pajak dan pengadilan).
+</p>
+</br>
+<p>
+Filing ialah Penyimpanan dokumen dalam bentuk hardcopy atau digital yang masih aktif diakses dalam waktu tertentu.  
+</p>
+</br>
+<p>
+Manajemen arsip mempunyai peranan penting bagi kelancaran operasional organisasi/perusahaan, yaitu sebagai sumber informasi atas runtutan kejadian (History) atas aktivitas organisasi. Arsip juga berperan sebagai alat bukti transaksi atau kegiatan bisnis yang menunjukkan akuntabilitas organisasi. Arsip berfungsi sebagai referensi informasi otentik jika dilakukan pemeriksaan, audit atau pembuktian secara hukum.
+</p>
+</div>
+
+	
+	</div> 
+
+<div>
+</section>
+
+
+
+
+
+
+<!-- Portfolio -->
+<section id="Portfolio" class="content"> 
+  
+  <!-- Container -->
+  <div class="container portfolio_title"> 
+    
+    <!-- Title -->
+    <div class="section-title">
+      <h2>Gallery</h2>
+	<h6><span></span></h6>
+	  
+    </div>
+    <!--/Title --> 
+    
+  </div>
+  <!-- Container -->
+  
+  <div class="portfolio-top"></div>
+  
+  <!-- Portfolio Filters -->
+  <div class="portfolio"> 
+    
+    
+    <!-- Portfolio Wrapper -->
+    <div class="isotope fadeInLeft animated wow grid" style="position: relative; overflow: hidden; height: 480px;" id="portfolio_wrapper">  
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four   animals isotope-item effect-oscar">
+	   
+        <div class="portfolio_img"> 
+		<img src="img/portfolio_pic1.jpg"  alt="Portfolio 1"> </div> 
+			<figcaption>		
+				<div>
+				  <a href="img/portfolio_pic1.jpg" class="fancybox"> 
+					<h2>Sistem Penyimpanan</h2>
+							<p></p>
+				  </a>
+				</div>
+			</figcaption>
+       </figure>
+      <!--/Portfolio Item --> 
+	  
+	  
+	  
+      
+      <!-- Portfolio Item-->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(337px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  wildlife isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/portfolio_pic2.jpg" alt="Portfolio 1"> </div>
+        	<figcaption>		
+				<div>
+				  <a href="img/portfolio_pic2.jpg" class="fancybox"> 
+					<h2>Rak Penyimpanan</h2>
+							<p></p>
+				  </a>
+				</div>
+			</figcaption>
+        </figure>
+      <!--/Portfolio Item --> 
+      
+      <!-- Portfolio Item -->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(674px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four  wildlife  isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/portfolio_pic3.jpg" alt="Portfolio 1"> </div>
+       <figcaption>		
+				<div>
+				  <a href="img/portfolio_pic3.jpg" class="fancybox"> 
+					<h2>Lokasi Kantor</h2>
+							<p></p>
+				  </a>
+				</div>
+			</figcaption>
+        </figure>
+      <!--/Portfolio Item--> 
+      
+      <!-- Portfolio Item-->
+      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(1011px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four    nature web isotope-item effect-oscar">
+        <div class="portfolio_img"> <img src="img/portfolio_pic4.jpg" alt="Portfolio 1"> </div>
+         <figcaption>		
+				<div>
+				  <a href="img/portfolio_pic4.jpg" class="fancybox"> 
+					<h2>Peta Lokasi </h2>
+							<p></p>
+				  </a>
+				</div>
+			</figcaption>
+      </figure>
+      <!-- Portfolio Item --> 
+
+    </div>
+    <!--/Portfolio Wrapper --> 
+    
+  </div>
+  <!--/Portfolio Filters -->
+  
+  <div class="portfolio_btm"></div>
+  
+  
+  <div id="project_container">
+    <div class="clear"></div>
+    <div id="project_data"></div>
+  </div>
+ 
+  
+</section>
+
+
+
+<!--Footer-->
+<footer class="footer_wrapper" id="contact">
+  <div class="container">
+    <section class="page_section contact" id="contact">
+      <div class="contact_section">
+        <h2>Contact Us</h2>
+
+		<h6>&nbsp;</h6>
+     
+      </div>
+      <div class="row">
+        <div class="col-lg-4 wow fadeInLeft">	
+		 <div class="contact_info">
+                            <div class="detail">
+                                <h4>Head Office</h4>
+                                <p>Komp. Gudang Indoserena Blok D NO 10,</p>
+								<p>Kel. Bunder, Cikupa, Kab. Tangerang - Banten</p>
+                            </div>
+                            <div class="detail">
+                                <h4>call me</h4>
+                                <p>+62 21 29324591</p>
+                            </div>
+                            <div class="detail">
+                                <h4>Email us</h4>
+                                <p>jasolusindo@java-arsip.com</p>
+                            </div> 
+                        </div>
+       		  
+			  
+			  
+			  
+			  
+			  
+          
+          <ul class="social_links">
+            <li class="twitter animated bounceIn wow delay-02s"><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+            <li class="facebook animated bounceIn wow delay-03s"><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li class="pinterest animated bounceIn wow delay-04s"><a href="#"><i class="fa fa-instagram"></i></a></li>
+			<!--
+            <li class="gplus animated bounceIn wow delay-05s"><a href="#"><i class="fa fa-linkedin"></i></a></li> 
+             <li class="gplus animated bounceIn wow delay-05s"><a href="#"><i class="fa fa-behance"></i></a></li> 
+			-->
+          </ul>
+		  
+		  
+		  
+		  
+        </div>
+        <div class="col-lg-8 wow fadeInLeft delay-06s">
+          <div class="form">
+            <input class="input-text" type="text" name="" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+            <input class="input-text" type="text" name="" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+            <textarea class="input-text text-area" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
+            <input class="input-btn" type="submit" value="send message">
+          </div>
+        </div>
+		
+		
+		
+		
+		
+      </div>
+    </section>
+  </div>
+  <div class="container">
+    <div class="footer_bottom"><span>Copyright © 2016 - <a href="#">Java Archiving Solusindo </a></span> </div>
+  </div>
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script> 
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.js"></script>
+<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script> 
+<script type="text/javascript" src="js/wow.js"></script> 
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
